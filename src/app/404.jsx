6c18d8a562bd4073e7b5e153c4f70b0094419d44
RyanLinconl <ryanlinconl1234@gmail.com>
2025-04-24 15:00:06 -0300

    adicionando tudo feito até agr

---
+++ src/app/404.jsx
@@ -0,0 +1,10 @@
+// app/404.jsx
+export default function NotFound() {
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>Página não encontrada</h1>
+        <p>Voltar para <a href="/">página inicial</a></p>
+      </div>
+    );
+  }
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,44 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { Categorias } from "./components/Categorias";
+import { Produtos } from "./components/Produtos";
 
-export default function Home() {
+
+async function fetchProdutosApi() {
+  const res = await fetch(
+     "https://api.npoint.io/5ce361b8a7fccfe63d6a/produtos"
+  );
+
+  if (!res.ok) {
+    throw new Error("Não foi possível obter os dados")
+  }
+
+  const produtos = await res.json();
+
+  return produtos;
+}
+
+async function fetchCategoriasApi() {
+  const res = await fetch("http://localhost:3000/api/categorias");
+
+  if (!res.ok) {
+    throw new Error("Não foi possível obter os dados")
+  }
+
+  const produtos = await res.json();
+
+  return produtos;
+}
+
+export default async function Home() {
+  const produtos = await fetchProdutosApi();
+  const { categorias } = fetchCategoriasApi();
+
   return (
-    <div className={styles.page}>
+    <>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <Categorias categorias={categorias} />
+        <Produtos produtos={produtos} />
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
