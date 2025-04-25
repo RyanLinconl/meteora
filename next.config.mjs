8ca0df80cadf9ff29a75b59b074d57aa88fbf40a
RyanLinconl <ryanlinconl1234@gmail.com>
2025-04-25 14:44:27 -0300

    v1.00

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        pathname: "**",
-      },
-    ],
+    loader: "custom",
+    loaderFile: "./src/app/imagem.js"
   },
 };
 
